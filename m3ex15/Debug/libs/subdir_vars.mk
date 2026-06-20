924fe956f03d1856c71e7f71e5235b6e02e9c5bf
Alan HenriqueRF <alhenrangelfera@gmail.com>
2026-06-20 20:22:11 -0300

    config

---
+++ m3ex15/Debug/libs/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../libs/i2c.c \
+../libs/lcd.c 
+
+C_DEPS += \
+./libs/i2c.d \
+./libs/lcd.d 
+
+OBJS += \
+./libs/i2c.obj \
+./libs/lcd.obj 
+
+OBJS__QUOTED += \
+"libs\i2c.obj" \
+"libs\lcd.obj" 
+
+C_DEPS__QUOTED += \
+"libs\i2c.d" \
+"libs\lcd.d" 
+
+C_SRCS__QUOTED += \
+"../libs/i2c.c" \
+"../libs/lcd.c" 
+
+
---
+++ m3ex18/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f5529.cmd 
+
+C_SRCS += \
+../m3ex17.c 
+
+C_DEPS += \
+./m3ex17.d 
+
+OBJS += \
+./m3ex17.obj 
+
+OBJS__QUOTED += \
+"m3ex17.obj" 
+
+C_DEPS__QUOTED += \
+"m3ex17.d" 
+
+C_SRCS__QUOTED += \
+"../m3ex17.c" 
+
+
